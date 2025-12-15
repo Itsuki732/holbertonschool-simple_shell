fcad6eba65caf50b95732b0bc2c6f8a51f712dcc
maxence30 <henous.maxence@outlook.fr>
2025-12-15 14:03:59 +0000

    simple_shell et shell.h (maxence's branch )

---
+++ simple_shell.c
@@ -0,0 +1,40 @@
+#include "shell.h"
+
+int main(void)
+{
+    char *line = NULL;
+    size_t len = 0;
+    pid_t pid;
+
+    while (1)
+    {
+        write(1, "#cisfun$ ", 9);
+
+        if (getline(&line, &len, stdin) == -1)
+        {
+            write(1, "\n", 1);
+            free(line);
+            return (0);
+        }
+
+        line[strcspn(line, "\n")] = '\0';
+
+        if (line[0] == '\0')
+            continue;
+
+        pid = fork();
+
+        if (pid == 0)
+        {
+            char *argv[] = { line, NULL };
+
+            execve(line, argv, environ);
+            perror("./shell");
+            exit(1);
+        }
+        else
+        {
+            wait(NULL);
+        }
+    }
+}
